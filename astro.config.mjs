e8e24922075518ec44a5d94f8454fe10e504a130
ringoldsdev <ringolds@lescinskis.com>
2025-05-08 23:46:50 +0300

    chore: added alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    alpinejs({ entrypoint: '/src/entrypoint.ts' })
+  ],
   vite: {
     plugins: [tailwindcss()],
   },

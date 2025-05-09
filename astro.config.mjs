772891b4ea55d217105b1ee66c2c5230cc94ba2f
ringoldsdev <ringolds@lescinskis.com>
2025-05-09 09:31:57 +0300

    chore: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,15 @@ import mdx from '@astrojs/mdx';
 
 import partytown from "@astrojs/partytown";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     alpinejs({ entrypoint: '/src/entrypoint.ts' }),
     mdx(),
     partytown(),
+    sitemap(),
   ],
   vite: {
     plugins: [tailwindcss()],

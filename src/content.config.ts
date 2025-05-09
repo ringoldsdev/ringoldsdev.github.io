2474962fc6d4e208012d411dad4b746f89e1829e
ringoldsdev <ringolds@lescinskis.com>
2025-05-09 09:49:41 +0300

    chore: set up rss

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,12 @@
 import { defineCollection, z } from "astro:content";
+import { rssSchema } from "@astrojs/rss";
 
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string().optional(),
-  }),
-  type: "content", // Ensure the collection type is explicitly set to 'content'
+	schema: rssSchema,
+	type: "content", // Ensure the collection type is explicitly set to 'content'
 });
 
 // Ensure MDX files are included in the content collection
 export const collections = {
-  blog,
+	blog,
 };

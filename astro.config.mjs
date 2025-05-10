b65073a6277dfb59815969d61f4d71d7ba35fa5e
ringoldsdev <ringolds@lescinskis.com>
2025-05-11 01:44:18 +0300

    chore: implemented custom astro components for mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,8 @@ import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 
 import sectionize from "@hbsnow/rehype-sectionize";
 
+import rehypeShiftHeading from "rehype-shift-heading";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ringoldsdev.github.io/",
@@ -30,8 +32,20 @@ export default defineConfig({
 		rehypePlugins: [
 			rehypeHeadingIds,
 			sectionize,
-			rehypeAutolinkHeadings,
-			// [rehypeAutolinkHeadings, { behavior: "wrap" }],
+			[rehypeShiftHeading, { shift: 1 }],
+			// 			[
+			// 				rehypeAutolinkHeadings,
+			// 				{
+			// 					behavior: "append",
+			// 					content: {
+			// 						type: Node,
+			// 						value: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+			//   <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+			// </svg>
+			// `,
+			// 					},
+			// 				},
+			// 			],
 		],
 	},
 });

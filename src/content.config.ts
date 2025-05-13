1b7d042e47a8d4a13a54d16e9a626ddc59a911d1
ringoldsdev <ringolds@lescinskis.com>
2025-05-14 00:11:28 +0300

    chore: added test page for blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,18 +9,7 @@ const blog = defineCollection({
 	}),
 });
 
-const tags = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		layout: z.string(),
-		tags: z.array(z.string()),
-		date: z.string(),
-		image: z.string().optional(),
-	}),
-});
-
 // Ensure MDX files are included in the content collection
 export const collections = {
 	blog,
-	tags,
 };

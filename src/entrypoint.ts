ed72c645f201c83b81fde5e31a3d9ca818181074
ringoldsdev <ringolds@lescinskis.com>
2025-05-29 22:54:57 +0300

    chore: persist dark mode

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,6 +1,8 @@
 import type { Alpine } from "alpinejs";
 import intersect from "@alpinejs/intersect";
+import persist from "@alpinejs/persist";
 
 export default (Alpine: Alpine) => {
 	Alpine.plugin(intersect);
+	Alpine.plugin(persist);
 };

e00128c79d3ab7774e398309c1d98f90b281bbee
ringoldsdev <ringolds@lescinskis.com>
2025-06-03 10:52:48 +0300

    chore: updated about me section to be an mdx file

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,7 +9,15 @@ const blog = defineCollection({
 	}),
 });
 
+const me = defineCollection({
+	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/me" }),
+	schema: z.object({
+		skills: z.array(z.string()),
+	}),
+});
+
 // Ensure MDX files are included in the content collection
 export const collections = {
 	blog,
+	me,
 };
